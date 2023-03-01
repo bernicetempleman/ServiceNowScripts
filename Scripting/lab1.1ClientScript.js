517c3b76b424ccf5f16fb68abd4abb832e110e8a
Bernice Templeman <bernice.templeman@gmail.com>
2023-02-28 20:27:41 -0500

    Add files via upload

---
+++ Scripting/lab1.1ClientScript.js
@@ -0,0 +1,44 @@
+// ServiceNow Client Script 
+// Code to test the Syntax Editor features
+// - Syntax coloring
+// - Special character highlighting
+// - Layout
+// - And more!
+
+var myString = "Hello World";
+var myNum = 32;
+var myObj = {
+	property1: "first",
+	propertyTwo: "second",
+	property3: "third"
+};
+
+for (var i = 0; i < myArray.length; i++){
+	alert("The current value of myArray is: " + myArray[i]);
+}
+// try <tab>
+try {
+//comment added here to prevent empty block statement alert
+}
+catch(err){
+g_form.addErrorMessage("A runtime error occurred: " + err);
+}
+
+/*
+Create a Syntax Editor Macro
+1. Navigate to System Definition > Syntax Editor Macros.
+2. Create a new macro:
+Name: try
+Comments: Client-side try/catch
+*/
+
+//help <tab>
+// The Syntax Editor macros are:
+// -----------------------------
+// try - Client-side try/catch
+// doc - Documentation Header 
+// for - Standard loop for arrays
+// vargror - Example GlideRecord Or Query
+// info - 
+// method - Standard JavaScript Class Method
+// vargr - A common pattern of creating and querying a GlideRecord
---
+++ Scripting/lab1.2ClientScript.js
@@ -0,0 +1,13 @@
+//ServiceNow Client Script used to practice Syntax Checking
+
+var peripherals = [keyboard, mouse, printer, speakers,];
+var myNum = .5;
+
+for (var i = 0; i < peripherals.length; i++){
+	alert("Current peripheral is: " + peripheral[i]);
+}
+
+if (myNum) {
+	myNum++;
+	alert("myNum: " + myNum);
+}
